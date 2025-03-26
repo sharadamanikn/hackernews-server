f08dd87d52edd81d32a26ab98d6bbf3ee8066708
Sharadamani K N <sharadamanikn@gmail.com>
2025-03-26 16:38:12 +0530

    Add initial server setup with Hono and JWT integration

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import "dotenv/config";
+import { jwtSecretKey } from '../environment';
+const app = new Hono()
+import jwt from "jsonwebtoken";
+import { allRoutes } from "./routes/routes";
+//console.log("JWT Secret Key:", jwtSecretKey);
+
+
+/*
+const payload: jwt.JwtPayload = {
+  iss: "https://purpleshorts.co.in",
+  sub: "Sharadamanikn",
+};
+const token = jwt.sign(payload, jwtSecretKey, {
+  algorithm: "HS256",
+  expiresIn: "30d", 
+});
+console.log("Token: ",token);
+try {
+  const decoded = jwt.verify(token, jwtSecretKey);
+  console.log("Decoded Payload", decoded);
+} catch (e) {
+  console.log("Error", e);
+}
+*/
+
+serve(allRoutes, (info) => {
+  console.log(`Server is running @ http://localhost:${info.port}`);
+});
+
