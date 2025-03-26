113838b3ed9d4ad2b59e9595b48eb8d70050e5df
Sharadamani K N <sharadamanikn@gmail.com>
2025-03-26 16:39:40 +0530

    Add user routes for fetching user details and listing users with pagination

---
+++ src/routes/users-routes.ts
@@ -0,0 +1,77 @@
+import { Hono } from "hono";
+import { prismaClient } from "../extras/prisma";
+import { tokenMiddleware } from "./middlewares/token-middleware";
+import { getAllUsers, getMe } from "../controllers/users/users-controller";
+import { GetMeError } from "../controllers/users/users-types";
+
+export const usersRoutes = new Hono();
+
+usersRoutes.get("/me", tokenMiddleware, async (context) => {
+  const userId = context.get("userId");
+
+  try {
+    const user = await getMe({
+      userId,
+    });
+
+    return context.json(
+      {
+        data: user,
+      },
+      200
+    );
+  } catch (e) {
+    if (e === GetMeError.BAD_REQUEST) {
+      return context.json(
+        {
+          error: "User not found",
+        },
+        400
+      );
+    }
+
+    return context.json(
+      {
+        message: "Internal Server Error",
+      },
+      500
+    );
+  }
+});
+
+
+// /users
+usersRoutes.get("", tokenMiddleware, async (context) => {
+    try {
+        const url = new URL(context.req.url); // Get query parameters
+        const page = parseInt(url.searchParams.get("page") || "1", 10);
+        const pageSize = parseInt(url.searchParams.get("pageSize") || "10", 10);
+    
+        if (page < 1 || pageSize < 1) {
+          return context.json({ message: "Invalid page or pageSize" }, 400);
+        }
+        
+    
+        const result = await getAllUsers(page, pageSize);
+    
+        return context.json(
+          {
+            data: result.users,
+            pagination: {
+              page,
+              pageSize,
+              total: result.total,
+              totalPages: Math.ceil(result.total / pageSize),
+            },
+          },
+          200
+        );
+      } catch (e) {
+        console.error("Error fetching users:", e);
+        return context.json({ message: "Internal Server Error" }, 500);
+      }
+});
+
+
+
+
