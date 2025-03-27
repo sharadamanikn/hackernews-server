6247d6a467cd6f2433b7220f232fd98ad68a6849
Sharadamani K N <sharadamanikn@gmail.com>
2025-03-27 11:01:11 +0530

    Add posts routes to main routing configuration

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { authenticationRoutes } from "./authentication-routes";
 import { usersRoutes } from "./users-routes";
 import { logger } from "hono/logger";
+import {postsRoutes} from "./posts-routes";
 
 export const allRoutes = new Hono();
 
@@ -9,4 +10,5 @@ allRoutes.use(logger());
 
 allRoutes.route("/auth", authenticationRoutes);
 allRoutes.route("/users", usersRoutes);
+allRoutes.route("/posts", postsRoutes);
 
