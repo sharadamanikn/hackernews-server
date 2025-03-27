4c77a83929ce6e1e85a0bd2f6fc4bfc90ca23d61
Sharadamani K N <sharadamanikn@gmail.com>
2025-03-27 11:01:04 +0530

    Add posts routes with createPost endpoint and error handling

---
+++ src/routes/posts-routes.ts
@@ -0,0 +1,22 @@
+import { Hono } from "hono";
+import { tokenMiddleware } from "./middlewares/token-middleware";
+import { createPost} from "../controllers/posts/posts-controller";
+import  {createPostError, type createPostResult} from "../controllers/posts/posts-types";
+
+export const postsRoutes = new Hono();
+
+postsRoutes.post("/", tokenMiddleware, async (c) => {
+    try {
+        const body = await c.req.json(); 
+    
+        if (!body.id || !body.title || !body.content || !body.authorId) {
+          return c.json({ message: createPostError.BAD_REQUEST }, 400);
+        }
+    
+        const newPost: createPostResult = await createPost(body);
+    
+        return c.json(newPost, 201); 
+      } catch (error) {
+        return c.json({ message: "Server Error" }, 400);
+      }
+});
