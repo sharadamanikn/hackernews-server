3fe461b975700dbc008a519ed2e69a221f409a8f
Sharadamani K N <sharadamanikn@gmail.com>
2025-03-27 22:52:57 +0530

    Add likes routes to main routing file

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,7 @@ import { authenticationRoutes } from "./authentication-routes";
 import { usersRoutes } from "./users-routes";
 import { logger } from "hono/logger";
 import {postsRoutes} from "./posts-routes";
+import {likesRoutes} from "./likes-routes";
 
 export const allRoutes = new Hono();
 
@@ -11,5 +12,6 @@ allRoutes.use(logger());
 allRoutes.route("/auth", authenticationRoutes);
 allRoutes.route("/users", usersRoutes);
 allRoutes.route("/posts", postsRoutes);
+allRoutes.route("/likes", likesRoutes);
 
 
