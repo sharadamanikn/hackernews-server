38e2d31f9d4ab649d2c09f9d9e5abed0d0186edc
Sharadamani K N <sharadamanikn@gmail.com>
2025-03-28 16:15:57 +0530

    Remove unnecessary blank line in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { serve } from '@hono/node-server'
 import "dotenv/config";
 import { allRoutes } from "./routes/routes";
 
-
 serve(allRoutes, (info) => {
   console.log(`Server is running @ http://localhost:${info.port}`);
 });
