a36f7ae48212879a2be6f3b2da2688562ae40fba
Sharadamani K N <sharadamanikn@gmail.com>
2025-03-28 11:52:50 +0530

    Add comments routes to main routing file

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,10 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 import { authenticationRoutes } from "./authentication-routes";
 import { usersRoutes } from "./users-routes";
-import { logger } from "hono/logger";
 import {postsRoutes} from "./posts-routes";
 import {likesRoutes} from "./likes-routes";
+import {commentsRoutes} from "./comments-routes";
 
 export const allRoutes = new Hono();
 
@@ -13,5 +14,6 @@ allRoutes.route("/auth", authenticationRoutes);
 allRoutes.route("/users", usersRoutes);
 allRoutes.route("/posts", postsRoutes);
 allRoutes.route("/likes", likesRoutes);
+allRoutes.route("/comments", commentsRoutes);
 
 
