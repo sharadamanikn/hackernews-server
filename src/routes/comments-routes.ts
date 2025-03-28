d79cf07ca07f11f9b683b02f0cc4edff1b80e1f6
Sharadamani K N <sharadamanikn@gmail.com>
2025-03-28 16:16:49 +0530

    Add delete and update comment routes with error handling

--- src/routes/comments-routes.ts
+++ src/routes/comments-routes.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { tokenMiddleware } from "./middlewares/token-middleware";
-import {  createCommentOnPost, getAllComments } from "../controllers/comments/comments-controller";
-import { CreateCommentError, GetCommentError }  from "../controllers/comments/comments-types";
+import {  createCommentOnPost, deleteCommentById, getAllComments, updateCommentById } from "../controllers/comments/comments-controller";
+import { CreateCommentError, DeleteCommentError, GetCommentError, UpdateCommentError }  from "../controllers/comments/comments-types";
 
 export const commentsRoutes = new Hono();
 
@@ -42,4 +42,51 @@ commentsRoutes.get("/on/:postId", tokenMiddleware, async(c) => {
         return c.json({ message: GetCommentError.INTERNAL_SERVER_ERROR }, 500);  
     }
 
+});
+
+
+commentsRoutes.delete("/:commentId", tokenMiddleware, async (c) => {
+    try {
+        const commentId = c.req.param("commentId");
+        const userId = c.get("userId");
+
+        if (!commentId || !userId) {
+            return c.json({ message: DeleteCommentError.BAD_REQUEST }, 400);
+        }
+
+        const result = await deleteCommentById({ commentId, userId });
+
+        if (result.error) {
+            return c.json({ message: result.error }, 403);
+        }
+
+        return c.json({ message: "Comment deleted successfully" }, 200);
+    } catch (e) {
+        console.error("Error deleting comment:", e);
+        return c.json({ message: DeleteCommentError.INTERNAL_SERVER_ERROR }, 500);
+    }
+});
+
+
+commentsRoutes.patch("/:commentId", tokenMiddleware, async (c) => {
+    try {
+        const commentId = c.req.param("commentId");
+        const userId = c.get("userId");
+        const { text } = await c.req.json();
+
+        if (!commentId || !userId || !text.trim()) {
+            return c.json({ message: UpdateCommentError.BAD_REQUEST }, 400);
+        }
+
+        const result = await updateCommentById({ commentId, userId, text });
+
+        if (result.error) {
+            return c.json({ message: UpdateCommentError.NOT_FOUND},403);
+        }
+
+        return c.json({ message: "Comment updated successfully" }, 200);
+    } catch (e) {
+        console.error("Error updating comment:", e);
+        return c.json({ message: UpdateCommentError.INTERNAL_SERVER_ERROR }, 500);
+    }
 });
