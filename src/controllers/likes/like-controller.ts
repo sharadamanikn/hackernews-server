54ca4e91f52165780fdd0c24c9bc96b8b74d73ee
Sharadamani K N <sharadamanikn@gmail.com>
2025-03-28 16:36:16 +0530

    Added final logic to the like implementation

--- src/controllers/likes/like-controller.ts
+++ src/controllers/likes/like-controller.ts
@@ -10,45 +10,35 @@ export const createLikeOnPost = async (parameters: { userId: string, postId: str
         postId: parameters.postId,
       },
     });
-  
     if (existingLike) {
       throw CreateLikeError.ALREADY_EXISTS; 
     }
-  
     const like = await prismaClient.like.create({
       data: {
         userId: parameters.userId,
         postId: parameters.postId,
       },
     });
+  return { like };
+};
   
-    return { like };
-  };
-  
-
-  export const getAllLikes = async (parameters: { postId: string, page: number, pageSize: number }): Promise<GetLikesResult> => {
+export const getAllLikes = async (parameters: { postId: string, page: number, pageSize: number }): Promise<GetLikesResult> => {
     const {postId,page,pageSize}= parameters;
     if(!postId)
     {
       throw GetLikesError.BAD_REQUEST;
     }
-
     const likes = await prismaClient.like.findMany({
       where: { postId: parameters.postId },
       orderBy: {createdAt: "desc"},
       skip: (page-1)*pageSize,
       take:pageSize,
     });
-  
     const totalLikes = await prismaClient.like.count({where:{postId}});
-  
     return { likes, totalLikes };
-  };
+};
   
-
-
-
-  export const deleteLikeOnPost = async (parameters: { userId: string, postId: string }): Promise<DeleteLikeResult> => {
+export const deleteLikeOnPost = async (parameters: { userId: string, postId: string }): Promise<DeleteLikeResult> => {
     const { userId, postId } = parameters;
     if (!userId || !postId) {
         throw new Error(DeleteLikeError.BAD_REQUEST);
@@ -62,7 +52,6 @@ export const createLikeOnPost = async (parameters: { userId: string, postId: str
     await prismaClient.like.delete({
         where: { id: existingLike.id },
     });
-
     return { message: "Like removed successfully" };
 };
 
