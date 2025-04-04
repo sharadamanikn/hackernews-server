b8639ecaa2c14cd305b190f42cd12c2681d52ae7
Sharadamani K N <sharadamanikn@gmail.com>
2025-04-04 15:04:02 +0530

    Add /info route to return a Hello World message

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { serve } from '@hono/node-server'
-import "dotenv/config";
+//import "dotenv/config";
 import { allRoutes } from "./routes/routes";
+
+
+allRoutes.get("/info", (context) => {
+  return context.json({
+    message: "Hello World",
+  });
+});
 
 serve(allRoutes, (info) => {
   console.log(`Server is running @ http://localhost:${info.port}`);
