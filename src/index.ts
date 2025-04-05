60472f7128b6eb443f5ed3416d371645db1e0d78
Sharadamani K N <sharadamanikn@gmail.com>
2025-04-05 09:33:00 +0530

    Refactor index.ts to remove Hono and swaggerUI integration, simplifying route handling

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { serve } from '@hono/node-server'
 //import "dotenv/config";
 import { allRoutes } from "./routes/routes";
-import { Hono } from 'hono';
-import { swaggerUI } from '@hono/swagger-ui';
 
-const app = new Hono();
 
-app.get('/ui', swaggerUI({ url: '/doc' }))
-export default app
+allRoutes.get("/info", (context) => {
+  return context.json({
+    message: "Hello World",
+  });
+});
 
 serve(allRoutes, (info) => {
   console.log(`Server is running @ http://localhost:${info.port}`);
