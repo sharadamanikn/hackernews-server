fc1d14f61bbd58938133d53a73356cba682ffd52
Sharadamani K N <sharadamanikn@gmail.com>
2025-04-05 11:57:07 +0530

    Refactor routes.ts to integrate swaggerUI for enhanced API documentation

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,19 +1,59 @@
-import { Hono } from "hono";
-import { logger } from "hono/logger";
-import { authenticationRoutes } from "./authentication-routes";
-import { usersRoutes } from "./users-routes";
-import {postsRoutes} from "./posts-routes";
-import {likesRoutes} from "./likes-routes";
-import {commentsRoutes} from "./comments-routes";
+import { Hono } from 'hono';
+import { swaggerUI } from '@hono/swagger-ui';
 
-export const allRoutes = new Hono();
+import { authenticationRoutes } from './authentication-routes';
+import { usersRoutes } from './users-routes';
+import { postsRoutes } from './posts-routes';
+import { likesRoutes } from './likes-routes';
+import { commentsRoutes } from './comments-routes';
 
-allRoutes.use(logger());
+const allRoutes = new Hono();
+const openapiRoutes = new Hono();
 
-allRoutes.route("/auth", authenticationRoutes);
-allRoutes.route("/users", usersRoutes);
-allRoutes.route("/posts", postsRoutes);
-allRoutes.route("/likes", likesRoutes);
-allRoutes.route("/comments", commentsRoutes);
+allRoutes.get('/ui', swaggerUI({ url: '/openapi.json' }));
+
+allRoutes.route('/auth', authenticationRoutes);
+allRoutes.route('/users', usersRoutes);
+allRoutes.route('/posts', postsRoutes);
+allRoutes.route('/likes', likesRoutes);
+allRoutes.route('/comments', commentsRoutes);
 
 
+allRoutes.get('/hello', (c) => {
+    return c.json({ message: 'Hello World!' });
+  });
+  
+allRoutes.get('/openapi.json', (c) =>
+    c.json({
+      openapi: '3.0.0',
+      info: {
+        title: 'My API',
+        version: '1.0.0',
+      },
+      paths: {
+        '/hello': {
+          get: {
+            summary: 'Say Hello',
+            responses: {
+              '200': {
+                description: 'Success',
+                content: {
+                  'application/json': {
+                    schema: {
+                      type: 'object',
+                      properties: {
+                        message: { type: 'string' },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    })
+  );
+
+export { allRoutes };
+export default openapiRoutes;
